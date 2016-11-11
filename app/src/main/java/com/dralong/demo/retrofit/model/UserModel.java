200fd606123221a0a4ac03bb0c8d240593b4dd84
JerryLong <ldralong@163.com>
2016-11-11 16:30:20 +0800

    retrofit 2.0 的简单使用

---
+++ app/src/main/java/com/dralong/demo/retrofit/model/UserModel.java
@@ -0,0 +1,7 @@
+package com.dralong.demo.retrofit.model;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public class UserModel {
+}
---
+++ app/src/main/java/com/dralong/demo/retrofit/service/ApiService.java
@@ -0,0 +1,7 @@
+package com.dralong.demo.retrofit.service;
+
+/**
+ * Created by Administrator on 2016/11/9.
+ */
+public class ApiService {
+}
